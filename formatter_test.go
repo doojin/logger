1208f69d5233f9c08a10d602b2961d0c3e40cd50
Dmitry Papka <dmitry.papka@gmail.com>
2014-09-01 12:07:15 +0000

    Test for Info and Warn formatter buildings added

--- formatter_test.go
+++ formatter_test.go
@@ -125,7 +125,67 @@ var testcases = []testcase{
 func Test_format_ShouldFormatCorrectly(t *testing.T) {
 	for i := range testcases {
 		testcase := testcases[i]
-		actualOutput := testcase.formatter.format(testcase.input, testcase.args...)
+		actualOutput := testcase.formatter.format(testcase.input, testcase.args)
 		assert.Equal(t, testcase.expectedOutput, actualOutput)
 	}
+}
+
+func Test_buildInfoFormatter_ShouldProvideCorrectFormatter(t *testing.T) {
+    logger := New()
+    formatter := buildInfoFormatter(logger.Settings)
+    input := "dummy"
+    
+    actualOutput := formatter.format(input, []interface{}{})
+    
+    assert.Equal(t, "[INFO] dummy", actualOutput)
+}
+
+func Test_buildInfolnFormatter_ShouldProvideCorrectFormatter(t *testing.T) {
+    logger := New()
+    formatter := buildInfolnFormatter(logger.Settings)
+    input := "dummy"
+    
+    actualOutput := formatter.format(input, []interface{}{})
+    
+    assert.Equal(t, "[INFO] dummy\n", actualOutput)
+}
+
+func Test_buildInfofFormatter_ShouldProvideCorrectFormatter(t *testing.T) {
+    logger := New()
+    formatter := buildInfolnFormatter(logger.Settings)
+    input := "dummy %v"
+    
+    actualOutput := formatter.format(input, []interface{}{"dummy"})
+    
+    assert.Equal(t, "[INFO] dummy dummy\n", actualOutput)
+}
+
+func Test_buildWarnFormatter_ShouldProvideCorrectFormatter(t *testing.T) {
+    logger := New()
+    formatter := buildWarnFormatter(logger.Settings)
+    input := "dummy"
+    
+    actualOutput := formatter.format(input, []interface{}{})
+    
+    assert.Equal(t, "[WARN] dummy", actualOutput)
+}
+
+func Test_buildWarnlnFormatter_ShouldProvideCorrectFormatter(t *testing.T) {
+    logger := New()
+    formatter := buildWarnlnFormatter(logger.Settings)
+    input := "dummy"
+    
+    actualOutput := formatter.format(input, []interface{}{})
+    
+    assert.Equal(t, "[WARN] dummy\n", actualOutput)
+}
+
+func Test_buildWarnfFormatter_ShouldProvideCorrectFormatter(t *testing.T) {
+    logger := New()
+    formatter := buildWarnfFormatter(logger.Settings)
+    input := "dummy %v"
+    
+    actualOutput := formatter.format(input, []interface{}{"dummy"})
+    
+    assert.Equal(t, "[WARN] dummy dummy\n", actualOutput)
 }
