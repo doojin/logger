462310ae5cc22dadc6a2bdfdcf56445d1b8802e8
Dmitry Papka <dmitry.papka@gmail.com>
2014-09-05 12:51:35 +0000

    Logging to file support

--- logger.go
+++ logger.go
@@ -15,14 +15,13 @@ type LoggerI interface {
 type Logger struct {
 	// Configuration settings
 	Settings settings
-	// Collection of writers
-	Writers map[string]io.Writer
 }
 
 type settings struct {
 	Writer     string
 	Layout     string
 	TimeFormat string
+	Filename   string
 }
 
 func New() *Logger {
@@ -32,18 +31,43 @@ func New() *Logger {
 			Layout:     "{time} [{level}] {message}",
 			TimeFormat: "15:04:05",
 		},
-
-		Writers: map[string]io.Writer{
-			"default": os.Stdout,
-			"console": os.Stdout,
-		},
 	}
 }
 
 func (l *Logger) Log(levelId string, message string, args ...interface{}) {
 	formattedMessage := l.formatMessage(levelId, message, args...)
 	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formattedMessage)
+	l.write(writer, formattedMessage)
+}
+
+func (l *Logger) Info(message string) {
+	formatter := buildInfoFormatter(l.Settings)
+	writer := l.getWriter(l.Settings.Writer)
+	l.write(writer, formatter.format(message))
+}
+
+func (l *Logger) Infoln(message string) {
+	formatter := buildInfolnFormatter(l.Settings)
+	writer := l.getWriter(l.Settings.Writer)
+	l.write(writer, formatter.format(message))
+}
+
+func (l *Logger) Infof(message string, args ...interface{}) {
+	formatter := buildInfofFormatter(l.Settings)
+	writer := l.getWriter(l.Settings.Writer)
+	l.write(writer, formatter.format(message, args...))
+}
+
+func (l *Logger) Warn(message string) {
+	formatter := buildWarnFormatter(l.Settings)
+	writer := l.getWriter(l.Settings.Writer)
+	l.write(writer, formatter.format(message))
+}
+
+func (l *Logger) Warnln(message string) {
+	formatter := buildWarnlnFormatter(l.Settings)
+	writer := l.getWriter(l.Settings.Writer)
+	l.write(writer, formatter.format(message))
 }
 
 func (l *Logger) formatMessage(levelId string, message string, args ...interface{}) (result string) {
@@ -64,45 +88,31 @@ func (l *Logger) formatMessage(levelId string, message string, args ...interface
 	return
 }
 
-func (l *Logger) Info(message string) {
-	formatter := buildInfoFormatter(l.Settings)
-	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formatter.format(message))
-}
-
-func (l *Logger) Infoln(message string) {
-	formatter := buildInfolnFormatter(l.Settings)
-	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formatter.format(message))
-}
-
-func (l *Logger) Infof(message string, args ...interface{}) {
-	formatter := buildInfofFormatter(l.Settings)
-	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formatter.format(message, args...))
-}
-
-func (l *Logger) Warn(message string) {
-	formatter := buildWarnFormatter(l.Settings)
-	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formatter.format(message))
-}
-
-func (l *Logger) Warnln(message string) {
-	formatter := buildWarnlnFormatter(l.Settings)
-	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formatter.format(message))
-}
-
 func (l *Logger) Warnf(message string, args ...interface{}) {
 	formatter := buildWarnfFormatter(l.Settings)
 	writer := l.getWriter(l.Settings.Writer)
-	io.WriteString(writer, formatter.format(message, args...))
+	l.write(writer, formatter.format(message, args...))
 }
 
-func (l *Logger) getWriter(key string) io.Writer {
-	if writer, ok := l.Writers[key]; ok {
-		return writer
+func (l *Logger) getWriter(writerName string) io.Writer {
+	switch writerName {
+	case "console":
+		return os.Stdout
+	case "file":
+		return l.getFileWriter()
+	default:
+		return os.Stdout
 	}
-	return l.Writers["default"]
+}
+
+func (l *Logger) getFileWriter() (file io.Writer) {
+	file, _ = os.OpenFile(l.Settings.Filename, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0777)
+	return
+}
+
+func (l *Logger) write(writer io.Writer, message string) {
+	io.WriteString(writer, message)
+	if l.Settings.Writer == "file" {
+		writer.(*os.File).Close()
+	}
 }
